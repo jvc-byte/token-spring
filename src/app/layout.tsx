0dbea0f3231c25d08965bf8f462a31d008ce89d5
jvc-byte <jvc8463@gmail.com>
2024-11-24 03:47:10 +0100

    + daisyui - Next.js Template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      <body data-theme="luxury"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -6,13 +7,12 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+
+  plugins: [
+    daisyui,
+  ],
+
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "forest", "synthwave", "night", "winter", "emerald", "valentine", "halloween", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
   },
-  plugins: [],
 } satisfies Config;
